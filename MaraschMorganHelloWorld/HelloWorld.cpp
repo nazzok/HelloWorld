f9b3e46d5338aceeb83e16740044e83d2ff2e5b9
nazzok <65244920+nazzok@users.noreply.github.com>
2021-10-19 14:17:44 -0400

    First comment

--- MaraschMorganHelloWorld/HelloWorld.cpp
+++ MaraschMorganHelloWorld/HelloWorld.cpp
@@ -1,3 +1,11 @@
+/*
+Morgan Marasch
+10/19/21
+Week 9: Source Code Dropbox
+Goal:
+Write "Hello, World" program, check it into Git, modify, and check the update.
+*/
+
 #include <iostream>
 using namespace std;
 int main()
